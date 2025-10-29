29a97de10df77513d62f7c45d666d52a471ba583
Pim Vermeer <pim.vermeer@gmail.com>
2025-10-29 19:16:12 +0100

    chore(login): fix shellcheck + add no internet message

--- .config/autostart/login.sh
+++ .config/autostart/login.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 script_dir=$(dirname "$0")
-script_name=$(basename $0)
+script_name=$(basename "$0")
 
 # Log everything to logger
-exec 1> >(logger -s -t $script_name) 2>&1
+exec 1> >(logger -s -t "$script_name") 2>&1
 
 function notify_error() {
   echo "'$1' failed to run"
@@ -13,18 +15,21 @@ function notify_error() {
 
 function run_command() {
   local app="$1"
+  local clean_string2
   local clean_string=${app//[\/]/\\/}
-  local clean_string2=$(echo $clean_string | awk -F "&& " '{ print $NF }')
+  clean_string2=$(echo "$clean_string" | awk -F "&& " '{ print $NF }')
   shift
-  $@ 1> >(sed "s/^/[$clean_string2] /") 2>&1
+  "$@" 1> >(sed "s/^/[$clean_string2] /") 2>&1
 }
 
 # Get login commands if defined
 login_commands_script="$script_dir/login_commands.sh"
 login_commands=()
-if [ -f $login_commands_script ]; then
+# shellcheck disable=SC1090
+if [ -f "$login_commands_script" ]; then
   echo "Sourcing $login_commands_script"
-  source $login_commands_script
+  # shellcheck disable=SC1090
+  source "$login_commands_script"
 else
   echo "No login commands found in $login_commands_script"
 fi
@@ -37,7 +42,7 @@ echo "Running $script_name"
 pids=""
 for app in "${start_apps[@]}"; do
   echo "Running: '$app'"
-  run_command "$app" eval $app || notify_error "$app" &
+  run_command "$app" eval "$app" || notify_error "$app" &
   pids="$pids $!"
 done
 
